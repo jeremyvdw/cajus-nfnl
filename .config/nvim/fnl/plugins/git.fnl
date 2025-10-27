13cdfb106d6d29f142ac97122ac5130cd0fbcda7
Jeremy Van de Wyngaert <jeremy.vandewyngaert@exoscale.com>
2025-10-27 14:35:28 +0100

    install tpope's vim -fugitive, -commentary and -projectionist

---
+++ .config/nvim/fnl/plugins/git.fnl
@@ -0,0 +1,6 @@
+[{1 :tpope/vim-fugitive
+  :lazy false}
+ {1 :tpope/vim-commentary
+  :lazy false}
+ {1 :tpope/vim-projectionist
+  :lazy false}]
---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,2 @@
+-- [nfnl] fnl/plugins/git.fnl
+return {{"tpope/vim-fugitive", lazy = false}, {"tpope/vim-commentary", lazy = false}, {"tpope/vim-projectionist", lazy = false}}
